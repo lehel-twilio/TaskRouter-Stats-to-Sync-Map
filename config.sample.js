5e7bcaae449a99a5c002d589d82478cc0fd72630
Lehel Gyeresi <lgyeresi@twilio.com>
2019-03-06 15:54:13 -0500

    Added sample config file

---
+++ config.sample.js
@@ -0,0 +1,16 @@
+module.exports = {
+  twilio: {
+    accountSid: 'AC...',
+    authToken: '...',
+    workspace: 'WS...',
+    syncService: 'IS...',
+    syncMap: 'MP...'
+  },
+  taskQueues: {
+    WQ...: 'Payments',
+    WQ...: 'NOCP',
+    WQ...: 'Inbound CSC',
+    WQ...: 'Everyone'
+  },
+  interval: 5000
+}
